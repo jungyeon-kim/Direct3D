0eb62d980c0edfcf81341b6e19c470d2a98b46c5
Kim Jung Yeon <zoemfhs123@gmail.com>
2020-09-26 02:21:44 +0900

    dx12 구조 공부
    dx12 구조를 공부하기 위해 샘플파일 다운

---
+++ 구조공부/Scene.h
@@ -0,0 +1,82 @@
+//-----------------------------------------------------------------------------
+// File: Scene.h
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include "Shader.h"
+#include "Player.h"
+
+struct LIGHT
+{
+	XMFLOAT4				m_xmf4Ambient;
+	XMFLOAT4				m_xmf4Diffuse;
+	XMFLOAT4				m_xmf4Specular;
+	XMFLOAT3				m_xmf3Position;
+	float 					m_fFalloff;
+	XMFLOAT3				m_xmf3Direction;
+	float 					m_fTheta; //cos(m_fTheta)
+	XMFLOAT3				m_xmf3Attenuation;
+	float					m_fPhi; //cos(m_fPhi)
+	bool					m_bEnable;
+	int						m_nType;
+	float					m_fRange;
+	float					padding;
+};
+
+struct LIGHTS
+{
+	LIGHT					m_pLights[MAX_LIGHTS];
+	XMFLOAT4				m_xmf4GlobalAmbient;
+};
+
+struct MATERIALS
+{
+	MATERIAL				m_pReflections[MAX_MATERIALS];
+};
+
+class CScene
+{
+public:
+    CScene();
+    ~CScene();
+
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	virtual void CreateShaderVariables(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+	virtual void UpdateShaderVariables(ID3D12GraphicsCommandList *pd3dCommandList);
+	virtual void ReleaseShaderVariables();
+
+	void BuildLightsAndMaterials();
+
+	void BuildObjects(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+	void ReleaseObjects();
+
+	ID3D12RootSignature *CreateGraphicsRootSignature(ID3D12Device *pd3dDevice);
+	ID3D12RootSignature *GetGraphicsRootSignature() { return(m_pd3dGraphicsRootSignature); }
+
+	bool ProcessInput(UCHAR *pKeysBuffer);
+    void AnimateObjects(float fTimeElapsed);
+    void Render(ID3D12GraphicsCommandList *pd3dCommandList, CCamera *pCamera=NULL);
+
+	void ReleaseUploadBuffers();
+
+	CPlayer						*m_pPlayer = NULL;
+
+protected:
+	ID3D12RootSignature			*m_pd3dGraphicsRootSignature = NULL;
+
+	CObjectsShader				*m_pShaders = NULL;
+	int							m_nShaders = 0;
+
+	LIGHTS						*m_pLights = NULL;
+
+	ID3D12Resource				*m_pd3dcbLights = NULL;
+	LIGHTS						*m_pcbMappedLights = NULL;
+
+	MATERIALS					*m_pMaterials = NULL;
+
+	ID3D12Resource				*m_pd3dcbMaterials = NULL;
+	MATERIAL					*m_pcbMappedMaterials = NULL;
+};
